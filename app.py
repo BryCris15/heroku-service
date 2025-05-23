8a71ccdfba2f29584bc7866e8992c0b7eb7b6642
BryCris15 <bcmalla@uce.edu.ec>
2025-05-23 00:26:05 -0500

    Cargar cambio para ver acción de workflow

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hola, saludos desde Heroku."
+    return "Hola, saludos desde Heroku. Primera carga de workflow"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
